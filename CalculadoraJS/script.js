c102427202983d16c0b1794e5c0cd0ab3a3b0af9
vspimentel <wyou55097@gmail.com>
2023-05-19 17:24:40 -0400

    Calculadora

--- CalculadoraJS/script.js
+++ CalculadoraJS/script.js
@@ -1,21 +1,82 @@
+v1 = 0; v2 = 0;
+
+pantalla_1 = document.getElementById("sec-1");
+pantalla_2 = document.getElementById("sec-2");
+
+igual = document.getElementById("igual");
+
 botones = document.getElementsByClassName("boton");
-pantalla = document.getElementById("pantalla");
 for (var i = 0; i < botones.length; i++) {
-    botones[i].onclick = function() {
+    botones[i].onclick = function () {
         escribir(this);
     }
 }
 
-function escribir(elemento){
+function escribir(elemento) {
     boton = elemento.innerHTML;
-    if (boton == "=") {
-        numeros = pantalla.innerHTML.split("+")
-        resultado = 0
-        numeros.forEach(numero => {
-            resultado = resultado + parseInt(numero)
-        });
-        pantalla.innerHTML = resultado
-    } else {
-        pantalla.innerHTML = pantalla.innerHTML + boton;
+    switch (boton) {
+        case "+":
+            v1 = parseFloat(pantalla_2.innerHTML);
+            pantalla_1.innerHTML = v1 + "+";
+            pantalla_2.innerHTML = "";
+            igual.onclick = function () {
+                sumar();
+            }
+            break;
+        case "-":
+            v1 = parseFloat(pantalla_2.innerHTML);
+            pantalla_1.innerHTML = v1 + "-";
+            pantalla_2.innerHTML = "";
+            igual.onclick = function () {
+                restar();
+            }
+            break;
+        case "x":
+            v1 = parseFloat(pantalla_2.innerHTML);
+            pantalla_1.innerHTML = v1 + "x";
+            pantalla_2.innerHTML = "";
+            igual.onclick = function () {
+                multiplicar();
+            }
+            break;
+        case "/":
+            v1 = parseFloat(pantalla_2.innerHTML);
+            pantalla_1.innerHTML = v1 + "/";
+            pantalla_2.innerHTML = "";
+            igual.onclick = function () {
+                dividir();
+            }
+            break;
+        case "DEL":
+            pantalla_1.innerHTML = "";
+            pantalla_2.innerHTML = "";
+            break
+        default:
+            pantalla_2.innerHTML += boton;
+            break
     }
+}
+
+function sumar() {
+    v2 = parseFloat(pantalla_2.innerHTML);
+    pantalla_1.innerHTML += v2;
+    pantalla_2.innerHTML = v1 + v2;
+}
+
+function restar() {
+    v2 = parseFloat(pantalla_2.innerHTML);
+    pantalla_1.innerHTML += v2;
+    pantalla_2.innerHTML = v1 - v2;
+}
+
+function multiplicar() {
+    v2 = parseFloat(pantalla_2.innerHTML);
+    pantalla_1.innerHTML += v2;
+    pantalla_2.innerHTML = v1 * v2;
+}
+
+function dividir() {
+    v2 = parseFloat(pantalla_2.innerHTML);
+    pantalla_1.innerHTML += v2;
+    pantalla_2.innerHTML = (v1 / v2).toFixed(2);
 }
--- LaboratorioBD/login.php
+++ LaboratorioBD/login.php
@@ -5,7 +5,7 @@
     $contrasena = $_POST["contrasena"];
     $sql = "SELECT U.nombres, U.idrol, R.descripcion FROM usuarios U INNER JOIN roles R ON U.idrol = R.id WHERE correo = '$correo' AND password = SHA1($contrasena)";
     $consulta = mysqli_query($con, $sql);
-    $usuario = mysqli_fetch_array($consulta);
+    $usuario = mysqli_query($consulta);
     if($usuario != null){
         $_SESSION["nombre"] = $usuario["nombres"];
         $_SESSION["idrol"] = $usuario["idrol"];
